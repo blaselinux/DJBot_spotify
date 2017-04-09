add5607336324f7025920a5b08e494df7e80a2f7
blaselinux <blaselinux@gmail.com>
2017-04-09 11:22:24 +0200

    Add standalone search for artist and track

--- main.js
+++ main.js
@@ -10,6 +10,140 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 const spawn = require('child_process').spawn;
 
+function searchTrack(rtrack){
+    rtrack = rtrack.toLowerCase();
+    var searchterm = rtrack;
+    console.log("searching spotify for " + searchterm + " ....");
+    var searchtype = "artist,track";
+    var options = {
+        method: 'GET',
+        url: "https://api.spotify.com/v1/search",
+        qs: {
+            q: searchterm.replace(/ /g, "+"),
+            type: searchtype,
+            market: "HU",
+            limit: 50
+        }
+    };
+    var selectedtrack, selectedartist, selectedpopular, selectedprev;
+    var data = [];
+
+    request(options, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var result = JSON.parse(body);
+            
+            if (result.tracks.items.length > 0) {
+                
+                console.log("For ciklus: " + result.tracks.items.length);
+                for (var i = 0; i < result.tracks.items.length; i++){
+                    selectedtrack = result.tracks.items[i];
+                    var ftrack = selectedtrack.name.toLowerCase();
+                    console.log(selectedtrack.name);
+                    if (ftrack === rtrack){
+                        for (var j = 0; j < result.tracks.items[i].artists.length; j++){
+                            selectedartist = result.tracks.items[i].artists[j];
+                            var fartist = selectedartist.name.toLowerCase();
+                            console.log("Artist: " + selectedartist.name + " Track: " + selectedtrack.name);
+                            console.log("Found : " + ftrack, " by ", fartist, result.tracks.items[i].popularity);
+                            console.log("selectedtrack.preview_url: "+ result.tracks.items[i].preview_url);
+                            selectedpopular = result.tracks.items[i].popularity;
+                            selectedprev = result.tracks.items[i].preview_url;
+                            data.push({"artist":selectedartist.name,"track":selectedtrack.name,"popularity":selectedpopular,"preview":selectedprev});
+                            break;
+                        }
+                    }
+                }
+                console.log("For ciklus vége");
+                console.log(data);
+                var choose = 0;
+                for (var i = 1; i < data.length; i++){
+                    if (data[choose].popularity < data[i].popularity){
+                        choose =i;
+                    }
+                }
+                console.log("=== Playing: " + data[choose].track + ", by " + data[choose].artist + " " + data[choose].popularity + " ===");
+                downloadFile(data[choose].preview);
+
+            } else {
+                console.log("no song found from spotify");
+		console.log("StatusCode: "+response.statusCode);
+                setTimeout(function() {}, 800);
+            }
+
+        } else {
+            console.log(error + " error" + response.statusCode);
+        }
+    });
+}
+
+
+function searchArtist(rartist){
+    rartist = rartist.toLowerCase();
+    var searchterm = rartist;
+    console.log("searching spotify for " + searchterm + " ....");
+    var searchtype = "artist,track";
+    var options = {
+        method: 'GET',
+        url: "https://api.spotify.com/v1/search",
+        qs: {
+            q: searchterm.replace(/ /g, "+"),
+            type: searchtype,
+            market: "HU",
+            limit: 50
+        }
+    };
+    var selectedtrack, selectedartist, selectedpopular, selectedprev;
+    var data = [];
+    
+    request(options, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var result = JSON.parse(body);
+            
+            if (result.tracks.items.length > 0) {
+                
+                console.log("For ciklus: " + result.tracks.items.length);
+                for (var i = 0; i < result.tracks.items.length; i++){
+                    selectedtrack = result.tracks.items[i];
+                    var ftrack = selectedtrack.name.toLowerCase();
+                    console.log(selectedtrack.name);
+                    for (var j = 0; j < result.tracks.items[i].artists.length; j++){
+                        selectedartist = result.tracks.items[i].artists[j];
+                        var fartist = selectedartist.name.toLowerCase();
+                        if (fartist === rartist){
+                            console.log("Artist: " + selectedartist.name + " Track: " + selectedtrack.name);
+                            console.log("Found : " + ftrack, " by ", fartist, result.tracks.items[i].popularity);
+                            console.log("selectedtrack.preview_url: "+ result.tracks.items[i].preview_url);
+                            selectedpopular = result.tracks.items[i].popularity;
+                            selectedprev = result.tracks.items[i].preview_url;
+                            data.push({"artist":selectedartist.name,"track":selectedtrack.name,"popularity":selectedpopular,"preview":selectedprev});
+                            break;
+                        }
+                    }
+                }
+                console.log("For ciklus vége");
+                console.log(data);
+                var choose = 0;
+                for (var i = 1; i < data.length; i++){
+                    if (data[choose].popularity < data[i].popularity){
+                        choose =i;
+                    }
+                }
+                console.log("=== Playing: " + data[choose].track + ", by " + data[choose].artist + " " + data[choose].popularity + " ===");
+                downloadFile(data[choose].preview);
+
+            } else {
+                console.log("no song found from spotify");
+		console.log("StatusCode: "+response.statusCode);
+                setTimeout(function() {}, 800);
+            }
+
+        } else {
+            console.log(error + " error" + response.statusCode);
+        }
+    });
+    
+}
+
 function searchSpotify(rartist, rtrack, rgenre) {
     rartist = rartist.toLowerCase();
     rtrack = rtrack.toLowerCase();
@@ -64,6 +198,7 @@ function searchSpotify(rartist, rtrack, rgenre) {
                         choose =i;
                     }
                 }
+                console.log("=== Playing: " + data[choose].track + ", by " + data[choose].artist + " " + data[choose].popularity + " ===");
                 downloadFile(data[choose].preview);
 
             } else {
@@ -149,6 +284,7 @@ function searchSpotifyTrack(rartist, rtrack, rgenre) {
                             choose = i;
                         }
                     }
+                    console.log("=== Playing: " + data[choose].track + ", by " + data[choose].artist + " " + data[choose].popularity + " ===");
                     downloadFile(data[choose].preview);
                 } else {
                     console.log("no song found from spotify");
@@ -209,4 +345,6 @@ function downloadFile(url) {
 }
 
 //searchSpotify("Tankcsapda", "üdvözöl a pokol", "rock");
-searchSpotifyTrack("michael jackson", "stranger in moscow", "rock");
+//searchSpotifyTrack("queen", "a kind of magic", "rock");
+//searchArtist("Queen");
+searchTrack("killing in the name");
